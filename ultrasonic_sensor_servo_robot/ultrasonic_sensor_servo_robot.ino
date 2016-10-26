7307c8fc2fc0308967aef1b19f3f932ed8eae347
Riccardo Ciprini <riccardociprini@gmail.com>
2016-10-26 15:38:27 +0200

    modify folders and files location

--- ultrasonic_sensor_servo_robot/ultrasonic_sensor_servo_robot.ino
+++ ultrasonic_sensor_servo_robot/ultrasonic_sensor_servo_robot.ino
@@ -33,19 +33,19 @@ void loop() {
 }
 
 void fermo(){
-  servo1.writeMicroseconds(1500); //fermo il primo servo motore
-  servo2.writeMicroseconds(1500); //fermo il secondo servo motore
+  servo1.writeMicroseconds(1500); //stop first servo
+  servo2.writeMicroseconds(1500); //stop second servo
 }
 
 
 void avanti(){
-  servo1.writeMicroseconds(2500); //faccio andare avanti il primo servo motore
-  servo2.writeMicroseconds(2500); //faccio andare avanti il secondo servo motore
+  servo1.writeMicroseconds(2500); //forward first servo
+  servo2.writeMicroseconds(2500); //forward second servo
 }
 
 
 void indietro(){
-  servo1.writeMicroseconds(1000); //faccio andare indietro il primo servo motore
-  servo2.writeMicroseconds(1000); //faccio andare indietro il secondo servo motore
+  servo1.writeMicroseconds(1000); //back first servo
+  servo2.writeMicroseconds(1000); //back second servo
 }
 
