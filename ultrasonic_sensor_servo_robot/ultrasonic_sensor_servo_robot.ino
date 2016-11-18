2278e1d06443231b8714e06b8f4e0c358cd5e055
Riccardo Ciprini <riccardociprini@gmail.com>
2016-11-18 15:34:20 +0100

    add function to select correct route and fix

--- ultrasonic_sensor_servo_robot/ultrasonic_sensor_servo_robot.ino
+++ ultrasonic_sensor_servo_robot/ultrasonic_sensor_servo_robot.ino
@@ -4,47 +4,96 @@
 #define TRIGGER_PIN 9
 #define ECHO_PIN 8
 #define MAX_DISTANCE 400
+int leftdist = 0;
+int rightdist = 0;
+int dist=0;
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
-Servo servo1;
-Servo servo2;
+Servo servo_left;
+Servo servo_right;
+Servo servo_find;
  
 void setup() {
-  servo1.attach(10);
-  servo2.attach(11);
+  servo_left.attach(10);
+  servo_right.attach(11);
+  servo_find.attach(12);
+  servo_find.write(90);
   Serial.begin(9600);
+  delay(700);
 }
  
 void loop() {
   
-  int lettura=0;
-  lettura=sonar.ping_cm(); //salvo sulla variabile lettura i CM del sensore ultrasuoni
+  dist=sonar.ping_cm(); //salvo sulla variabile lettura i CM del sensore ultrasuoni
   
-  Serial.print(lettura); //stampo i valori sul monitor
-  Serial.println("cm"); //stampo i valori sul monitor
+  //Serial.print(lettura); //I write the values on the monitor
+  //Serial.println("cm");
   
-  if(lettura>20){
-    avanti();
+  if(dist<10){
+    findroute();
   }
   else{
-    fermo();
+    forward();
   }
   delay(100);
 }
 
-void fermo(){
-  servo1.writeMicroseconds(1500); //stop first servo
-  servo2.writeMicroseconds(1500); //stop second servo
+void alt(){
+  servo_left.writeMicroseconds(1500); //stop function servo
+  servo_right.writeMicroseconds(1500);
 }
 
 
-void avanti(){
-  servo1.writeMicroseconds(2500); //forward first servo
-  servo2.writeMicroseconds(2500); //forward second servo
+void forward(){
+  servo_left.writeMicroseconds(2500); //forward first servo
+  servo_right.writeMicroseconds(2500);
+}
+
+void turnleft () {
+  servo_left.writeMicroseconds(2500);    //correct the direction<----------------
+  servo_right.writeMicroseconds(2500);   //correct the direction
+  delay(1000);
+  alt();
+  return;
+}
+
+void turnright () {
+  servo_left.writeMicroseconds(2500);    //correct the direction<----------------
+  servo_right.writeMicroseconds(2500);   //correct the direction
+  delay(1000);
+  alt();
+  return;
+}
+
+void findroute(){
+  alt();
+  lookleft();
+  lookright();
+  if(leftdist < rightdist){
+    turnleft();
+  }
+  else{
+    turnright();
+  }
 }
 
 
-void indietro(){
-  servo1.writeMicroseconds(1000); //back first servo
-  servo2.writeMicroseconds(1000); //back second servo
+//functions to find the correct direction
+void lookleft() {
+  servo_find.write(150);
+  delay(700);
+  leftdist = sonar.ping_cm();
+  servo_find.write(90);
+  delay(700);
+  return;
 }
+
+void lookright () {
+  servo_find.write(30);
+  delay(700);
+  rightdist = sonar.ping_cm();
+  servo_find.write(90);                                  
+  delay(700);
+  return;
+}
+// end function
